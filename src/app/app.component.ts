82c5ee00e47d7e1ed8890d3a44569b907c24de84
Nabeel <n.siddiqui25@gmail.com>
2025-01-30 23:33:33 -0600

    add components and setup routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'getonedev-angular';
+  title = 'GetOneDev, LLC';
 }
