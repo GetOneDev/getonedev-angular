b7df7dc42de93c5f4073358991fc5048dc6f7eab
Nabeel <n.siddiqui25@gmail.com>
2025-02-01 20:37:16 -0600

    Sprint 5: Added contact page with form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  formData = {
+    name: '',
+    email: '',
+    inquiryType: '',
+    message: ''
+  };
 
+  onSubmit() {
+    console.log('Form Data:', this.formData);
+
+    alert('Thank you for your message! We will get back to you soon.');
+  }
 }
